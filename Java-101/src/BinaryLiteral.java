1071cabcfdd21d5be83805a033b4861cb42ab7e3
Ankush Thakur <ankush@bonamisoftware.com>
2020-08-07 00:25:33 +0530

    hello world commit.

---
+++ Java-101/src/BinaryLiteral.java
@@ -0,0 +1,12 @@
+
+public class BinaryLiteral {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 0B101001001010;
+
+		System.out.println("The binary value of 101 is " + i);
+	}
+
+}
---
+++ Java-101/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+		int i, j, k;
+		i = 4;
+		j = 5;
+		k = i + j;
+
+		System.out.println("the sum of " + i + " and " + j + " is " + k);
+	}
+
+}
